0e5405470e5405693b647cc11f31a9d93ade4c3a
Ramil <ramil.mukhametgaliev.07@mail.ru>
2025-12-24 20:27:14 +0300

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-	experimental: {
-		turbo: false,
+module.exports = {
+	output: 'export', // ВАЖНО: включаем статический экспорт
+	images: {
+		unoptimized: true, // чтобы картинки работали
 	},
+	// Если проект не в корне репозитория:
+	basePath: process.env.NODE_ENV === 'production' ? '/имя-репозитория' : '',
+	assetPrefix: process.env.NODE_ENV === 'production' ? '/имя-репозитория/' : '',
 }
-export default nextConfig;
